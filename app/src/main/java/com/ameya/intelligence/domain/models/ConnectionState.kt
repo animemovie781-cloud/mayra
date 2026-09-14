350f41ae512035f584aa2d547faefd1997368359
kanhadhiwar38-sketch <kanhadhiwar38@gmail.com>
2026-09-14 19:42:21 +0530

    kanha

---
+++ app/src/main/java/com/ameya/intelligence/domain/models/ConnectionState.kt
@@ -0,0 +1,11 @@
+package com.ameya.intelligence.domain.models
+
+/**
+ * Connection state for remote IDE sessions.
+ * Moved to domain layer to decouple UI from implementation.
+ */
+enum class ConnectionState {
+    DISCONNECTED,
+    CONNECTING,
+    CONNECTED
+}
